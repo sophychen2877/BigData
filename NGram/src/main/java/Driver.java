4fcd5187799a3bbdef912b5d6732b915b4631253
sophy.chen <sophy.chen@indexexchange.com>
2020-05-25 11:42:59 -0400

    first mapperrducer job for the ngram

---
+++ NGram/src/main/java/Driver.java
@@ -0,0 +1,37 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.db.DBConfiguration;
+import org.apache.hadoop.mapreduce.lib.db.DBOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+
+public class Driver {
+    public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+        //job1
+        Configuration conf1 = new Configuration();
+
+        conf1.set("noGram", args[2]);
+
+        Job job1 = Job.getInstance();
+        job1.setJobName("NGram");
+        job1.setJarByClass(Driver.class);
+
+        job1.setMapperClass(NGramLibraryBuilder.NGramMapper.class);
+        job1.setReducerClass(NGramLibraryBuilder.NGramReducer.class);
+
+        job1.setOutputKeyClass(Text.class);
+        job1.setOutputValueClass(IntWritable.class);
+
+        job1.setInputFormatClass(TextInputFormat.class);
+        job1.setOutputFormatClass(TextOutputFormat.class);
+
+        TextInputFormat.setInputPaths(job1, new Path(args[0]));
+        TextOutputFormat.setOutputPath(job1, new Path(args[1]));
+        job1.waitForCompletion(true);
+}
